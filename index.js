d1cddd9215550c790b0b1599ae0f08222e36c5d9
Taiwo <taiwotunde32@gmail.com>
2023-05-11 22:28:41 +0100

    update

--- index.js
+++ index.js
@@ -1,10 +1,28 @@
 
 const express = require("express");
 const app = express();
-const productRoute = require("./Routes/product.route");
+const productRoute = require("./Routes/product.route")
+const mongoose = require("mongoose");
+const {product} = require("./ProductSchema/ProductSchema.js");
 
+
+
+
+mongoose.connect("mongodb://localhost:27017/RESTFUL-API" ,{
+    useNewUrlParser: true, useUnifiedTopology: true 
+}).then( ()=>{
+    console.log("Connected")
+}).catch( (error)=>{
+    console.log("Failed to connect")
+})
 
 app.use("/products", productRoute)
+
+app.use((req, res)=>{
+res.status(404)
+res.send({error:"Product not found"})
+})
+
 
 
 
